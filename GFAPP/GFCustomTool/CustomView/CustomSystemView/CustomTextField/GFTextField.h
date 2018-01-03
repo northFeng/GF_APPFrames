124103369c848bf0524932e9da3260bd0100b39b
高亚峰 <XinKun@north-feng.local>
2018-01-03 17:45:01 +0800

    添加新文件夹

---
+++ GFAPP/GFCustomTool/CustomView/CustomSystemView/CustomTextField/GFTextField.h
@@ -0,0 +1,13 @@
+//
+//  GFTextField.h
+//  GFAPP
+//
+//  Created by XinKun on 2018/1/3.
+//  Copyright © 2018年 North_feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFTextField : UITextField
+
+@end
---
+++ GFAPP/GFCustomTool/CustomView/CustomSystemView/CustomTextField/GFTextField.m
@@ -0,0 +1,21 @@
+//
+//  GFTextField.m
+//  GFAPP
+//
+//  Created by XinKun on 2018/1/3.
+//  Copyright © 2018年 North_feng. All rights reserved.
+//
+
+#import "GFTextField.h"
+
+@implementation GFTextField
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
