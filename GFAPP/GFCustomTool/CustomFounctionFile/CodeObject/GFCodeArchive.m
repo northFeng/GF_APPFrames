6b20c405d387608bf92500f107156c8e9879fb3f
XinKun <XinKun@north-feng.local>
2018-02-18 12:36:38 +0800

    添加归档解档宏定义

--- GFAPP/GFCustomTool/CustomFounctionFile/CodeObject/GFCodeArchive.m
+++ GFAPP/GFCustomTool/CustomFounctionFile/CodeObject/GFCodeArchive.m
@@ -10,6 +10,40 @@
 
 #import <objc/runtime.h>
 
+//归档宏
+#define encodeRuntime(A) \
+\
+unsigned int count = 0;\
+Ivar *ivars = class_copyIvarList([A class], &count);\
+for (int i = 0; i<count; i++) {\
+Ivar ivar = ivars[i];\
+const char *name = ivar_getName(ivar);\
+NSString *key = [NSString stringWithUTF8String:name];\
+id value = [self valueForKey:key];\
+[encoder encodeObject:value forKey:key];\
+}\
+free(ivars);\
+\
+
+//解档宏
+#define initCoderRuntime(A) \
+\
+if (self = [super init]) {\
+unsigned int count = 0;\
+Ivar *ivars = class_copyIvarList([A class], &count);\
+for (int i = 0; i<count; i++) {\
+Ivar ivar = ivars[i];\
+const char *name = ivar_getName(ivar);\
+NSString *key = [NSString stringWithUTF8String:name];\
+id value = [decoder decodeObjectForKey:key];\
+[self setValue:value forKey:key];\
+}\
+free(ivars);\
+}\
+return self;\
+\
+
+
 @implementation GFCodeArchive
 
 - (void)encodeWithCoder:(NSCoder *)aCoder{
