0cd87ca846660f80c1dac1bf0b35034fd1e1e260
gaoyafeng <yafeng.gao@ishansong.com>
2018-09-17 11:14:06 +0800

    添加封装弹框视图

---
+++ GFAPP/GFCustomTool/CustomView/CustomToolView/AlertView/GFBaseAlertView.h
@@ -0,0 +1,52 @@
+//
+//  GFBaseAlertView.h
+//  GFAPP
+//  弹框基类视图
+//  Created by gaoyafeng on 2018/9/17.
+//  Copyright © 2018年 North_feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFBaseAlertView : UIView
+
+///底部视图
+@property (nonatomic,strong) UIView *backView;
+
+///bvWidth
+@property (nonatomic,assign) CGFloat bvWidth;
+
+///bvHeight
+@property (nonatomic,assign) CGFloat bvHeight;
+
+///width
+@property (nonatomic,assign) CGFloat sWidth;
+
+///height
+@property (nonatomic,assign) CGFloat sHeight;
+
+
+/**
+ *  @brief 创建视图
+ *
+ *  @param bvWidth backView的宽
+ *  @param bvHeight backView的高
+ *  @param width self的宽
+ *  @param height self的高
+ *  @return GFBaseAlertView
+ */
++ (GFBaseAlertView *)initWithBackViewWidth:(CGFloat)bvWidth bvHeight:(CGFloat)bvHeight selfWidth:(CGFloat)width selfHeight:(CGFloat)height;
+
+
+
+///弹出
+- (void)showAlertView;
+
+
+
+///隐藏
+- (void)hideAlertView;
+
+
+
+@end
---
+++ GFAPP/GFCustomTool/CustomView/CustomToolView/AlertView/GFBaseAlertView.m
@@ -0,0 +1,80 @@
+//
+//  GFBaseAlertView.m
+//  GFAPP
+//
+//  Created by gaoyafeng on 2018/9/17.
+//  Copyright © 2018年 North_feng. All rights reserved.
+//
+
+#import "GFBaseAlertView.h"
+
+@implementation GFBaseAlertView
+
+
++ (GFBaseAlertView *)initWithBackViewWidth:(CGFloat)bvWidth bvHeight:(CGFloat)bvHeight selfWidth:(CGFloat)width selfHeight:(CGFloat)height{
+    
+    GFBaseAlertView *alertView = [[GFBaseAlertView alloc] init];
+    
+    alertView.bvWidth = bvWidth;
+    alertView.bvHeight = bvHeight;
+    alertView.sWidth = width;
+    alertView.sHeight = height;
+    
+    return alertView;
+}
+
+
+#pragma mark - 视图布局
+//初始化
+- (instancetype)init{
+    
+    if ([super init]) {
+        
+        self.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.4];
+        self.backView = [[UIView alloc] initWithFrame:CGRectMake(0, self.sHeight, _bvWidth, _bvHeight)];
+        self.backView.backgroundColor = [UIColor whiteColor];
+        [self addSubview:self.backView];
+        [self createView];
+    }
+    return self;
+}
+
+
+//创建视图 && 必须都贴在backView上
+- (void)createView{
+    
+    
+    
+}
+
+
+
+///弹出
+- (void)showAlertView{
+    
+    self.hidden = NO;
+    [UIView animateWithDuration:0.2 animations:^{
+        self.backView.frame = CGRectMake(0, self.sHeight - self.bvHeight, self.bvWidth, self.bvHeight);
+    } completion:^(BOOL finished) {
+        
+    }];
+    
+}
+
+///隐藏
+- (void)hideAlertView{
+    
+    [UIView animateWithDuration:0.2 animations:^{
+        self.backView.frame = CGRectMake(0, self.sHeight, self.bvWidth, self.bvHeight);
+    } completion:^(BOOL finished) {
+        self.hidden = YES;
+    }];
+    
+}
+
+
+
+
+
+
+@end
