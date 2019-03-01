b052444100f4d36f91994f928a18e0561bde9823
gaoyafeng <yafeng.gao@ishansong.com>
2019-03-01 11:28:34 +0800

    添加手动冲突解决方法

--- GFAPP/GFCustomTool/CustomFounctionFile/GFMapFunction/FRMapViewVC.h
+++ GFAPP/GFCustomTool/CustomFounctionFile/GFMapFunction/FRMapViewVC.h
@@ -133,3 +133,149 @@ NS_ASSUME_NONNULL_BEGIN
 
 
 NS_ASSUME_NONNULL_END
+
+#pragma mark - 滑动冲突解决
+/**
+- (void)createView{
+    
+    //创建scrollView
+    self.scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, kTopNaviBarHeight, kScreenWidth, kScreenHeight - kTopNaviBarHeight)];
+    self.scrollView.delegate = self;
+    _scrollView.backgroundColor = APPColor_White;
+    _scrollView.clipsToBounds = YES;
+    //_scrollView.bounces = NO;
+    _scrollView.contentSize = CGSizeMake(kScreenWidth, kScreenHeight - kTopNaviBarHeight);//根据内容去计算
+    [self.view addSubview:_scrollView];
+    
+    CGRect backRect;
+    //判断状态
+    if (_orderCurrentStatus == APPEnumOrderState_complete || _orderCurrentStatus == APPEnumOrderState_cancle) {
+        
+        backRect = CGRectMake(0, 0, kScreenWidth, kScreenHeight - kTopNaviBarHeight);
+    }else{
+        //创建地图
+        FSMapViewVC *mapVC = [[FSMapViewVC alloc] init];
+        //设置地图
+        mapVC.gesturesTap = NO;
+        mapVC.gesturesRotate = NO;
+        mapVC.gesturesScroll = YES;
+        mapVC.gesturesOverlook = NO;
+        
+        [self addChildViewController:mapVC];
+        mapVC.view.frame = CGRectMake(0, 0, kScreenWidth, kScreenHeight - kTopNaviBarHeight);
+        [self.scrollView addSubview:mapVC.view];
+        
+        _mapVC = mapVC;
+        _mapView = mapVC.view;
+        
+        //添加手势
+        // 给主scrollview 添加手势
+        UIGestureRecognizer *mainScrollVTap = [[UIGestureRecognizer alloc]init];
+        mainScrollVTap.delegate = self;
+        [self.scrollView addGestureRecognizer:mainScrollVTap];
+        //手势随便加
+        UITapGestureRecognizer *provinceMapTap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(provinceMapTapped)];
+        provinceMapTap.delegate = self;
+        [self.mapVC.mapView addGestureRecognizer:provinceMapTap];
+        
+        //刷新按钮
+        _btnRefresh = [UIButton buttonWithType:UIButtonTypeCustom];
+        [_btnRefresh setImage:ImageNamed(@"map_sx") forState:UIControlStateNormal];
+        [self.mapVC.mapView addSubview:_btnRefresh];
+        _btnRefresh.frame = CGRectMake(kScreenWidth - 44, (kScreenHeight - kTopNaviBarHeight) - 200 - 68, 34, 34);
+        [_btnRefresh addTarget:self action:@selector(onClickBtnRefresh) forControlEvents:UIControlEventTouchUpInside];
+        
+        backRect = CGRectMake(0, (kScreenHeight - kTopNaviBarHeight) - 200, kScreenWidth, kScreenHeight);
+        
+        //添加信息视图尾部视图
+        [self.scrollView addSubview:self.footView];
+    }
+    
+    //添加backView
+    _backView = [[FSOrderContentView alloc] initWithFrame:backRect];
+    _backView.backgroundColor = [UIColor clearColor];
+    _backView.clipsToBounds = YES;
+    [_scrollView addSubview:_backView];
+    
+    if (_footView) {
+        //设置约束 ——>为backView的尾部跟随视图
+        self.footView.sd_layout.leftEqualToView(self.scrollView).rightEqualToView(self.scrollView).topSpaceToView(self.backView, 0).heightIs(kScreenHeight);
+    }
+    
+    //添加取消弹框 和 添加小费弹框
+    [self addCancleAlertView];
+    
+    //backView的回调事件
+    APPWeakSelf;
+    //待支付——->超时自动取消
+    _backView.blockAutoCancle = ^(BOOL result, id idObject) {
+        
+        [weakSelf autoCancleOrder];
+    };
+    
+}
+
+///渐变视图
+- (UIView *)footView{
+    
+    if (!_footView) {
+        _footView = [[UIView alloc] init];
+        _footView.backgroundColor = APPColor_White;
+    }
+    return _footView;
+}
+
+#pragma mark - 地图和scrollView冲突解决
+///地图添加的手势
+- (void)provinceMapTapped{
+    
+}
+///手势触摸之前就会触发这个代理
+-(BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldReceiveTouch:(UITouch *)touch{
+    
+    //判断如果是百度地图的view 既可以实现手势拖动 scrollview 的滚动关闭
+    
+    if ([gestureRecognizer.view isKindOfClass:[BMKMapView class]] ){
+        
+        self.scrollView.scrollEnabled = NO;
+        
+        return YES;
+    }else{
+        
+        self.scrollView.scrollEnabled = YES;
+        
+        return NO;
+    }
+    
+}
+
+//不断改变地图的显示
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView{
+    
+    CGFloat y = self.scrollView.contentOffset.y;
+    
+    //使地图的位置固定
+    self.mapView.frame = CGRectMake(0, y, kScreenWidth, kScreenHeight - kTopNaviBarHeight);
+}
+
+#pragma mark - 添加渐变视图
+///添加竖向的混合颜色
+- (void)view_addHybridBackgroundColorShowOnView:(UIView *)onView{
+    CAGradientLayer *gradientLayer = [CAGradientLayer layer];
+    UIColor *color1 = [APPColor_White colorWithAlphaComponent:0.0];
+    UIColor *color2 = [APPColor_White colorWithAlphaComponent:0.2];
+    UIColor *color3 = [APPColor_White colorWithAlphaComponent:0.3];
+    UIColor *color4 = [APPColor_White colorWithAlphaComponent:0.5];
+    UIColor *color5 = [APPColor_White colorWithAlphaComponent:1.0];
+    gradientLayer.colors = @[(__bridge id)color1.CGColor, (__bridge id)color2.CGColor, (__bridge id)color3.CGColor, (__bridge id)color4.CGColor, (__bridge id)color5.CGColor];
+    gradientLayer.locations = @[@0.3, @0.5, @0.7, @0.9, @1.0];
+    gradientLayer.startPoint = CGPointMake(0, 0);
+    gradientLayer.endPoint = CGPointMake(0, 1);
+    gradientLayer.frame = CGRectMake(0, 0, onView.frame.size.width, onView.frame.size.height);
+    
+    //[onView.layer addSublayer:gradientLayer];
+    [onView.layer insertSublayer:gradientLayer atIndex:0];
+}
+ 
+ */
+
