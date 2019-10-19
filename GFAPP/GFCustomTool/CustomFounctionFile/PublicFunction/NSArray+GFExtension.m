620b7433c8b058251603a08ba005ab596cac0b37
峰 <north_feng@sina.cn>
2019-10-19 18:03:26 +0800

    添加runtime方法交换的实例

--- GFAPP/GFCustomTool/CustomFounctionFile/PublicFunction/NSArray+GFExtension.m
+++ GFAPP/GFCustomTool/CustomFounctionFile/PublicFunction/NSArray+GFExtension.m
@@ -8,7 +8,43 @@
 
 #import "NSArray+GFExtension.h"
 
+#import <objc/runtime.h>
+
 @implementation NSArray (GFExtension)
+
++ (void)load {
+    Method fromMethod = class_getInstanceMethod(objc_getClass("__NSArrayI"), @selector(objectAtIndex:));
+    Method toMethod = class_getInstanceMethod(objc_getClass("__NSArrayI"), @selector(cm_objectAtIndex:));
+    method_exchangeImplementations(fromMethod, toMethod);
+}
+
+// 为了避免和系统的方法冲突，我一般都会在swizzling方法前面加前缀
+- (id)cm_objectAtIndex:(NSUInteger)index {
+    // 判断下标是否越界，
+    if ((self.count > 0) && (index >= 0) && (index <= self.count - 1)) {
+        // 如果没有问题，则正常进行方法调用
+        /**
+        里面调用了自身？这是递归吗？其实不是。这个时候方法替换已经有效了，cm_objectAtIndex这个SEL指向的其实是原来系统的objectAtIndex:的IMP。因而不是递归。
+         */
+        /**
+         1、这里调自己，其实是调用了 交换的那个方法
+         2、根据业务需求，在这里进行自己的业务处理，置于下面这步是否调用交互的那个方法，看需求而定！！
+         */
+        return [self cm_objectAtIndex:index];//这里不是自己调用自己，而是调用的那个交换方法！！！！！！
+    }else {
+        //如果越界就进入异常拦截
+        @try {
+            return [self cm_objectAtIndex:index];//这里不是自己调用自己，而是调用的那个交换方法！！！！！！
+        }
+        @catch (NSException *exception) {
+            // 在崩溃后会打印崩溃信息。如果是线上，可以在这里将崩溃信息发送到服务器
+            NSLog(@"---------- %s Crash Because Method %s  ----------\n", class_getName(self.class), __func__);
+            NSLog(@"%@", [exception callStackSymbols]);
+            return nil;
+        }
+        @finally {}
+    }
+}
 
 - (id)gf_getItemWithIndex:(NSInteger)index{
     
